5ffcd99bc516c385a2e7a5c01125d93e36af735e
Zoheb Rahiman <rahiman.zoheb@gmail.com>
2022-01-30 22:57:35 -0800

    Fix the README file

--- README.md
+++ README.md
@@ -4,4 +4,18 @@ Spring Boot application to play a simple Cows and Bulls guessing game!
 
 You have to guess a 4 letter word by providing alphabet one by one
 
+## Definitions
+Cow = A correct letter in wrong place
+Bull = A correct letter in correct place
 
+## Reference
+[Wikipedia Link](https://en.wikipedia.org/wiki/Bulls_and_Cows)
+
+## Steps to set up
+### Run back end app
++ Clone the repository [cowsandbulls](https://github.com/rahiman-zoheb/cowsandbulls)
++ Run it so that it is available on 127.0.0.1:8080 (proxy set up)
+### Run front end app
++ Clone the repository [cowsandbulls_reactjs](https://github.com/rahiman-zoheb/cowsandbulls_reactjs)
++ Run `npm install`
++ Run `npm start`
--- src/main/java/com/zohebrahiman/cowsandbulls/controller/GameController.java
+++ src/main/java/com/zohebrahiman/cowsandbulls/controller/GameController.java
@@ -2,10 +2,7 @@ package com.zohebrahiman.cowsandbulls.controller;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Collection;
 import java.util.Optional;
-
-import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,8 +12,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
